04a65e570f7ca05fa6ea0742cb480dff9f35f64f
banusriprakash <banusriprakash317@gmail.com>
2024-04-20 17:40:24 +0530

    feat: String reverse

---
+++ stringrev.java
@@ -0,0 +1,19 @@
+public class stringrev {
+    public static void main(String[] args) {
+        String str = "Banu sri prakash";
+
+        str.trim();
+
+        String words[] = str.split("\\s+");
+
+        StringBuilder s = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            s.append(words[i]);
+            if (i > 0) {
+                s.append(" ");
+            }
+        }
+        System.out.println(s.toString());
+    }
+}
